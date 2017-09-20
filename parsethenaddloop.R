8131dbf61934cc16971670f11ba3978e18b26598
Sarah <cloud.srhrnkn@fastmail.fm>
2017-09-20 12:15:06 -0400

    gender lookup via wikipedia

--- parsethenaddloop.R
+++ parsethenaddloop.R
@@ -1,16 +1,19 @@
 #this script looks for new BTB interviews and adds them to the dataset
+#simplified to stop manually inputting gender info - just asks if it's an author or not, then gets the gender data from wikipedia
 
 #General outline:
 #bring in new version of interviewee log
 #hit against current list to look for new interviewees
 #for each new interviewee
-  #bring in text file 
-  #look for potential author names
-  #loop through names and enter gender, flag errors
-  #loop through errors & fix
-#wind up with list of author names, genders, interviewees
+#bring in text file 
+#look for potential author names
+#loop through names and flag if real name, flag text errrs
+#loop through errors & fix
+#wind up with list of author names, interviewees
 #hit goodreads for data re new interviewees and new authors, add to GRdata
 #error checking for erroneous hits - look for cases where name and GR name are too far apart, manually fix
+#hit wikipedia for residual gender data, birthdate data
+#error check for data still missing
 #merge GRdata and author/interview listing to create new btb analysis dataset with interviewee & author 
 
 #load libraries ####
@@ -19,6 +22,9 @@ library("tidyverse", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resou
 library("lubridate", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 library("stringdist", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 library("stringr", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+library("rvest", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+library("WikipediR", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+library("tidytext", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 
 #create functions ####
 nF<-function(x) {ifelse(is.na(x), F,x)}
@@ -75,6 +81,83 @@ authdetails<-function(authname){
   
 }
 
+#function wikigetdata - get birthdate and gender info from wikipedia, based on a vector of names (assumes GR names)
+
+wikigetdata<-function(namevector){
+  wikidata<-data.frame(GRID=numeric(),GRname=character(),wikibirthdate=as.Date(x = integer(0), origin = "1970-01-01"),wikigender=character(),wikimsg=character())
+  for(i in 1:length(namevector)){
+    name<-namevector[i]
+    GRID<-NA
+    birthdate<-NA
+    gender<-NA
+    msg<-NA
+    try({
+      wikifull<- page_content(language = "en",project = "wikipedia",page_name = name)
+    })
+    if(!exists("wikifull")){
+      msg<-"no wiki page"} else{
+        if(wikifull$parse$text$`*` %>% read_html() %>% xml_text() %>% str_detect("This disambiguation page")){
+          msg<-"disambiguation"
+        } else{
+          tables<- wikifull$parse$text$`*` %>% read_html() %>%  xml2::xml_find_all(".//table")
+          if(length(tables)==0){
+            msg<-"no tables"
+          } else {
+            if(is.na(wikibiotablenum(wikifull))) {
+              msg<-"no bio table"
+            }
+            else {
+              table<-tables[[wikibiotablenum(wikifull)]] %>%  html_table(fill = T)
+              names(table)<-c("item","data")
+              birthdate<-table %>% filter(item=="Born") %>% select(data) %>% str_extract(pattern = "\\d+-\\d+-\\d+") %>% parse_date_time("ymd") %>% as.Date()
+            }
+          }
+        }
+        
+        assign(x = paste0("wikifull",str_replace_all(string = name,pattern = " ",replacement = "_")),value = wikifull,envir = .GlobalEnv)
+        assign(x = paste0("wikitable",str_replace_all(string = name,pattern = " ",replacement = "_")),value = table,envir = .GlobalEnv)
+        
+        temptext<-data.frame(line=1, text=wikifull$parse$text$`*` %>% read_html %>% xml_text(),stringsAsFactors = F)
+        temptokens<-temptext %>% unnest_tokens(output = word,input = text)
+        temptokens$row<-as.numeric(rownames(temptokens))
+        shehercount<-temptokens %>% filter(word %in% c("she","She","her","Her")) %>% count() %>% pull()
+        hehimcount<-temptokens %>% filter(word %in% c("he","He","him","Him")) %>% count() %>% pull()
+        if(nz(shehercount)==nz(hehimcount)){
+          gender<-NA
+        } else if(nz(shehercount)>nz(hehimcount)){
+          gender<-("f")
+        } else {
+          gender<-("m")
+        }
+      }
+    
+    tempdetails<-data.frame(GRID=GRdata$id[match(name,GRdata$GRname)],GRname=name,wikibirthdate=birthdate,wikigender=gender,wikimsg=msg,stringsAsFactors = F)
+    wikidata<-rbind(wikidata,tempdetails)
+    rm(wikifull)
+  }
+  return(wikidata)
+}
+
+
+#find the bio table within a wikipedia page - used in above function
+wikibiotablenum<- function(pagecontent){
+  try(findborn<-unlist(lapply(X = (pagecontent$parse$text$`*` %>% read_html() %>%  xml2::xml_find_all(".//table") %>%  html_table(fill = T)),FUN = function(x){max(grepl("Born",x[[1]]))})))
+  if(exists("findborn")){
+    if(sum(findborn)==0){
+      biotablenum<-NA 
+    } else {
+      biotablenum<-which.max(findborn)
+    }
+    return(biotablenum)
+  } else {
+    return(NA)
+  }
+}
+
+
+
+
+
 # bring in new version of log, find new interviewee names ####
 intervieweesnew<-read.csv("logscripted.csv")
 intervieweesnew$Date<-as.Date(x = intervieweesnew$Date,format = "%b. %d, %Y")
@@ -84,11 +167,11 @@ intervieweesnew<-intervieweesnew[which(!intervieweesnew$Subject %in% interviewee
 #for each interviewee:
 #read in file 
 #use regex to find potential author names
-#loop through author names and record genders
+#loop through author names and record: is a name (y), need attention (/), not a name (enter)
 #store issues for further look
 #wind up with list of author names, genders, interviewees
 
-authorsnew<-data.frame(name=character(),gender=character(),interviewee=character())
+authorsnew<-data.frame(name=character(),isauth=character(),interviewee=character())
 for(i in 1:length(intervieweesnew$Subject)){
   #read in file 
   interviewee<-as.character(intervieweesnew$Subject[i])
@@ -103,7 +186,7 @@ for(i in 1:length(intervieweesnew$Subject)){
   
   #first look for multi - store only if something is found in the line
   print(paste0("New interviewee: ",interviewee))
-  print("type gender; add slash at end if unsure or error in string")
+  print("type y if is an author name; type slash if unsure or error in string")
   for(x in 1:length(btblines)){
     tempmulti<-str_extract_all(string = btblines[x],pattern = "((?<![“])([:upper:]{1}(\\. )?)+[:lower:]+(?=([ \\’\\'-][:upper:]{1}(\\. )?)+)(?:[\\s\\’\\'-][:upper:]{1}(\\. )?[[:upper:]{1}([:lower:]\\'+)-]+)+)")[[1]]
     tempmulti<-str_replace(string = tempmulti,pattern = "\\.$",replacement = "")
@@ -117,52 +200,54 @@ for(i in 1:length(intervieweesnew$Subject)){
   }
   
   #var to store genders
-  multigend = character()
+  isauth = character()
   
-  #loop through multi and register genders
+  #loop through multi and register authorness
   for(x in 1:length(multi)){
-    tempmultigend<-readline(prompt = paste0(multi[x]," "))
-    multigend<-c(multigend, tempmultigend)
+    tempmulti<-readline(prompt = paste0(multi[x]," "))
+    isauth<-c(isauth, tempmulti)
   }
   
   #bring together names, genders
-  multiframe<-data.frame(name=multi,gender=multigend,stringsAsFactors = F)
-  multiframe<-multiframe[which(multiframe$gender!=""),]
+  multiframe<-data.frame(name=multi,isauth,stringsAsFactors = F)
+  multiframe<-multiframe[which(multiframe$isauth!=""),]
   
   #for single just view and tag any that are real
-  singlegend = character()
+  singleisauth = character()
   print(single)
   singleindices<-readline(prompt = "Which items are authors? enter index numbers separated by spaces")
   singleindices<-as.integer(unlist(str_split(string = singleindices," ")))
+  print("Enter y if author name ok, / if needs editing")
   for(x in 1:length(singleindices)){
-    tempsinglegend<-readline(prompt = paste0(single[singleindices[x]]," "))
-    singlegend<-c(singlegend, tempsinglegend)
+    tempsingle<-readline(prompt = paste0(single[singleindices[x]]," "))
+    singleisauth<-c(singleisauth, tempsingle)
   }
   
-  singleframe<-data.frame(name=single[singleindices],gender=singlegend,stringsAsFactors = F)
+  singleframe<-data.frame(name=single[singleindices],isauth=singleisauth,stringsAsFactors = F)
   authorsnew<-rbind(authorsnew, rbind(cbind(multiframe,interviewee=interviewee),cbind(singleframe,interviewee=interviewee)))
 }
 
 #fix flagged
 
 #find rownums of flagged entries
-fixnums<-grep("/",authorsnew$gender)
+fixnums<-grep("/",authorsnew$isauth)
 #loop through flagged entries and prompt for name,gender string - separated by comma, no quotes
-print("for each author enter correct name and gender, separated by comma, no quotes. if not an author just enter comma")
+print("for each author enter correct name, no quotes")
 for(i in 1:length(fixnums)){
   tempfix<-readline(prompt = paste(authorsnew[fixnums[i],],collapse = " "))
-  authorsnew[fixnums[i],1:2]<-unlist(str_split(string = tempfix,pattern = ","))
+  authorsnew$name[fixnums[i]]<-tempfix
+  authorsnew$isauth[fixnums[i]]<-"y"
 }
 #delete any blank lines
-authorsnew<-authorsnew[authorsnew$name!=""&authorsnew$gender!="",]
+authorsnew<-authorsnew[authorsnew$name!=""&authorsnew$isauth!="",]
 
 #hit goodreads for data re new interviewees and new authors, add to GRdata ####
 
 #record row count of GRdata so can look only at new entries later. If GRdata doesn't exist yet (ie, starting from scratch), create it
 if(exists("GRdata")){
-GRdatarowcountold<-nrow(GRdata)} else{
-  GRdata<-data_frame(name=character(),id=numeric(), gender=factor(levels = c("","female","male")), birthdate=as.Date(x = integer(0), origin = "1970-01-01"), town=character(),is.interviewee=logical(),is.author=logical(),input.gender=factor(levels=c("f","m")),gender.use=factor(levels=c("f","m")), GRname=character(),stringdistance=numeric(),matchOK=logical())
-}
+  GRdatarowcountold<-nrow(GRdata)} else{
+    GRdata<-data_frame(name=character(),id=numeric(), gender=factor(levels = c("","female","male")), birthdate=as.Date(x = integer(0), origin = "1970-01-01"), town=character(),is.interviewee=logical(),is.author=logical(),input.gender=factor(levels=c("f","m")),gender.use=factor(levels=c("f","m")), GRname=character(),stringdistance=numeric(),matchOK=logical())
+  }
 
 
 #loop through interviewees to get GR data
@@ -183,11 +268,13 @@ for (i in 1:length(authorstemp)){
   if(!authorstemp[i] %in% GRdata$name){
     tempdetails<-authdetails(authorstemp[i])
     tempdetails$birthdate<-as.Date(tempdetails$birthdate,format = "%Y/%m/%d")
-    GRdata<-rbind(GRdata,cbind(tempdetails,is.interviewee=NA,is.author=T,input.gender=authorsnew$gender[match(authorstemp[i],authorsnew$name)],gender.use=ifelse(test = is.na(tempdetails$gender)|tempdetails$gender==""|substr(tempdetails$gender,1,1)==authorsnew$gender[match(authorstemp[i],authorsnew$name)],yes = as.character(authorsnew$gender[match(authorstemp[i],authorsnew$name)]),no = substr(tempdetails$gender,1,1)),stringdistance=stringdist(a = tempdetails$name,b = tempdetails$GRname),matchOK=ifelse(test = stringdist(a = tempdetails$name,b = tempdetails$GRname)<3,yes = T,no = NA)))
+    GRdata<-rbind(GRdata,cbind(tempdetails,is.interviewee=NA,is.author=T,input.gender=NA,gender.use=NA,stringdistance=stringdist(a = tempdetails$name,b = tempdetails$GRname),matchOK=ifelse(test = stringdist(a = tempdetails$name,b = tempdetails$GRname)<3,yes = T,no = NA)))
   } else{
     GRdata$is.author[which(GRdata$name==authorstemp[i])]<-T
   }
 }
+
+
 
 # quality control: find the sketchy GR matches (do this before btb merge) ####
 mismatches<-GRdata %>% filter(row(GRdata[,1])>GRdatarowcountold&GRdata$stringdistance>0)  %>% data.frame
@@ -195,20 +282,32 @@ mismatches<-GRdata %>% filter(row(GRdata[,1])>GRdatarowcountold&GRdata$stringdis
 #if not too many, just eyeball and designate appropriately
 GRdata[which(GRdata$name %in% mismatches$name),"matchOK"]<-c()
 
-#or can use this:
-# #go through each level of distance and look for mismatches
-# i<-3 #starts at 3 - lower ok
-# mismatches[which(mismatches$stringdistance==i),] #look at mismatches at that level
-# mismatches$matchOK[which(mismatches$name=="")]<-F #set any that are wrong to F
-# mismatches$matchOK[which(mismatches$stringdistance==i&is.na(mismatches$matchOK))]<-T #set the rest at that level to T
-# i<-i+1 #increment up
-# #Then bring these designations back into GRdata
-# GRdata$matchOK[which(GRdata$name %in% mismatches$name)]<-mismatches$matchOK[match( GRdata$name[GRdata$name %in% mismatches$name],mismatches$name)]
-# #at some point clean these up, for now, throw them out at merge
+#get wikipedia gender###
+wikinew<-wikigetdata(namevector = GRdata$GRname[(GRdatarowcountold+1):length(GRdata$GRname)])
+
+
+#add missing genders
+#find rownums of flagged entries
+fixnums<-which(is.na(wikinew$wikigender))
+#loop through flagged entries and prompt for name,gender string - separated by comma, no quotes
+print("for each author enter m, f, or nothing")
+for(i in 1:length(fixnums)){
+  tempfix<-readline(prompt = paste0(wikinew$GRname[fixnums[i]]," "))
+  wikinew$wikigender[fixnums[i]]<-tempfix
+}
+
+#add gender and bday data to GRnames
+#for bday only the ones where GR data doesn't have bday
+GRdata$birthdate[which((GRdata$id %in% wikinew$GRID)&is.na(GRdata$birthdate))]<-wikinew$wikibirthdate[match(GRdata$id[which((GRdata$id %in% wikinew$GRID)&is.na(GRdata$birthdate))],wikinew$GRID)]
+#for gender, the wiki data takes the place of input gender, then evaluate
+GRdata$input.gender[which(GRdata$id %in% wikinew$GRID)]<-wikinew$wikigender[match(GRdata$id[which(GRdata$id %in% wikinew$GRID)],wikinew$GRID)]
+GRdata$gender.use[which(GRdata$id %in% wikinew$GRID)]<-case_when(GRdata$gender[which(GRdata$id %in% wikinew$GRID)]=="" ~ as.character(GRdata$input.gender[which(GRdata$id %in% wikinew$GRID)]), T ~ substr(GRdata$gender[which(GRdata$id %in% wikinew$GRID)],1,1))
+
 
 #add new interviewee and author names to crosswalk ####
 interviewees<-rbind(interviewees,cbind(intervieweesnew,GRID=GRdata$id[match(intervieweesnew$Subject,GRdata$name)]))
-authors<-rbind(authors,cbind(authorsnew, GRID=GRdata$id[match(authorsnew$name,GRdata$name)]))
+authorsnew$gender<-NA
+authors<-rbind(authors,cbind(authorsnew[,c(1,3:4)], GRID=GRdata$id[match(authorsnew$name,GRdata$name)]))
 
 
 #create dataset of authors & interviewees merging by GR ID (omits anyone who wasn't a GR hit) ####
@@ -223,9 +322,24 @@ btb<-merge(btb,interviewees[,c("GRID","Date")],by.x="interviewee.id",by.y="GRID"
 btb<-unique(btb)
 
 #save btb so as to load in rmd file.  ####
-#save GRdata just for safekeeping
+#save GRdata, authgr, searchgr, wiki files just for safekeeping - iterate update
+if(exists("savenum")){
+  savenum<-savenum+1} else {
+    savenum<-1
+  }
+
 save(btb,file = "btb.Rdata")
-save(GRdata,file="GRdata.Rdata")  
+save(btb,file = paste0("btb",as.character(savenum),".Rdata"))
+save(GRdata,"GRdata.Rdata")  
+save(GRdata,file= paste0("GRdata",as.character(savenum),".Rdata"))  
+save(list = apropos("authgr"),file = paste0("authgr",as.character(savenum),".Rdata"))
+save(list = apropos("searchgr"),file = paste0("searchgr",as.character(savenum),".Rdata"))
+save(list = apropos("wikifull"),file = paste0("wikifull",as.character(savenum),".Rdata"))
+save(list = apropos("wikitable"),file = paste0("wikitable",as.character(savenum),".Rdata"))
+rm(list = apropos("authgr"))
+rm(list = apropos("searchgr"))
+rm(list = apropos("wikifull"))
+rm(list = apropos("wikitable"))
 
 
 
